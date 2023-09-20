a97c3184ef7c06ac2c571eb5960eb99ebfd17ebf
kimberly moreau <moreau.magali.kimberly@gmail.com>
2023-09-20 11:31:14 +0200

    refactor: :bento: update js file
    add two functions for the navigation sidebar (open/close for the responsive)

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+// Fonction pour ouvrir la sidebar avec la navigation
+function openNav() {
+    document.getElementById("Sidenav").style.display = "block";
+}
+
+// Fonction pour fermer la sidebar avec la navigation
+function closeNav() {
+    document.getElementById("Sidenav").style.display = "none";
+} 
